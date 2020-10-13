8abb6d7a66c6c02e974d971c7beb82a9da88b0d8
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-13 12:36:33 +0200

    Segundo Commit

--- ContarDígitos/src/ContarDígitos.java
+++ ContarDígitos/src/ContarDígitos.java
@@ -3,8 +3,14 @@ public class ContarDígitos {
 
 	public static void main(String[] args) {
 		
-		int dígitos = 0;
-		Scanner teclado = new Scanner (System.in);
+		/*CONTAREMOS LOS DÍGITOS QUE TIENE UN NÚMERO INTRODUCIDO POR EL USUARIO.
+		/EJEMPLO: NÚMERO = 240
+		/NÚMERO DE DÍGITOS = 3 (2 4 0)
+		/PARA ELLO, DIVIDIREMOS ENTRE 10 TANTAS VECES COMO HAGA FALTA PARA LLEGAR A 0
+		*/
+		
+		int dígitos = 0;		//INICIALIZAR EL CONTADOR DE DÍGITOS
+		Scanner teclado = new Scanner (System.in);		//CREAR OBJETO DE TIPO SCANNER
 		System.out.print("Introduzca número entero: ");		//SOLCITA NÚMERO
 		int número = teclado.nextInt();		//INTRODUCE NÚMERO
 		
@@ -14,6 +20,6 @@ public class ContarDígitos {
 		}
 		while (número!=0);		//MIENTRAS EL NÚMERO NO SEA 0
 		
-		System.out.printf("El número tiene %d dígitos.",dígitos);
+		System.out.printf("El número tiene %d dígitos.",dígitos);		//OUTPUT
 	}
 }
